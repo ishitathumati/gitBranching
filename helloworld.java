1724dda9012bb833bcce77e01efb38f058085aa4
Ishita Thumati <ithumati@email.sc.edu>
2019-10-07 16:00:42 -0400

    first commit

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+package assignments.gitBranching;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
