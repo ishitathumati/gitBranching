76c8771b3eb28395fc2f8d2167d6dfe620fdc16d
Ishita Thumati <ithumati@email.sc.edu>
2019-10-07 16:12:26 -0400

    change three

--- helloworld.java
+++ helloworld.java
@@ -6,6 +6,10 @@ public class helloworld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("change");
+		int a=2;
+		int b=2;
+		int c=a+b;
+		System.out.println(+a+ " + " +b+ " = " +c);
 	}
 
 }
